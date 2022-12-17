bbb062940cf3866ba6ecb862f4e30b013bc97ca7
Charlotte Taboada <lovdeirdre@gmail.com>
2022-12-17 11:37:54 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,14 +1,46 @@
 package LeapYear;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class LeapYearGUI extends JFrame {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
-    public static void main(String[] args) {
+    public LeapYearGUI() {
+        setTitle("Leap Year Checker");
+
+        btnCheckYear.addActionListener((event) -> {
+                try {
+                    int year = Integer.parseInt(tfYear.getText());
+
+                    //Formula
+                    boolean isLeapYear = false;
+                    if (year % 4 == 0) {
+                        if (year % 100 == 0) {
+                            if (year % 400 == 0) {
+                                isLeapYear = true;
+                            }
+                        } else {
+                            isLeapYear = true;
+                        }
+                    }
+
+                    String message = isLeapYear ? "Leap year" : "Not a leap year";
+                    JOptionPane.showMessageDialog(null, message);
+
+                }catch (NumberFormatException lot) {
+                        JOptionPane.showMessageDialog(null, "Please input a valid year.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+
+        });
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        LeapYearGUI disp = new LeapYearGUI();
+        disp.setContentPane(new LeapYearGUI().panel1);
+        disp.setSize(250, 300);
+        disp.setVisible(true);
     }
 }
