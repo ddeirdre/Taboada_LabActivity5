ec1f5a85f0ba5c2a97f5a2c3ad9108d9c67c2ecf
Charlotte Taboada <lovdeirdre@gmail.com>
2022-12-17 14:46:30 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -18,7 +18,34 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    public FoodOrderGUI() {
+        setTitle("Food Ordering System");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rbNone);
+        bg.add(rb5);
+        bg.add(rb10);
+        bg.add(rb15);
+            btnOrder.addActionListener((event) -> {
+                double price = 0.0;
+                if (cPizza.isSelected()) price += 100;
+                if (cBurger.isSelected()) price += 80;
+                if (cFries.isSelected()) price += 65;
+                if (cSoftDrinks.isSelected()) price += 55;
+                if (cTea.isSelected()) price += 50;
+                if (cSundae.isSelected()) price += 40;
+                if (rb5.isSelected()) price -= price * 0.05;
+                else if (rb10.isSelected()) price -= price * 0.10;
+                else if (rb15.isSelected()) price -= price * 0.15;
+
+                JOptionPane.showMessageDialog(panel1, "The total price is Php " + String.format("%.2f", price));
+                });
+                    setLocationRelativeTo(null);
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
     public static void main(String[] args) {
         FoodOrderGUI dsp = new FoodOrderGUI();
+        dsp.setContentPane(new FoodOrderGUI().panel1);
+        dsp.setSize(600, 500);
+        dsp.setVisible(true);
     }
 }
