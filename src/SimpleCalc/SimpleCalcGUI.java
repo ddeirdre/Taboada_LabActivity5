15614d8370946e3f2fca93da9ca9a3b6d5293dc6
Charlotte Taboada <lovdeirdre@gmail.com>
2022-12-17 11:48:39 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,12 +1,19 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JLabel lblResult;
+
+    public static void main(String[] args) {
+        SimpleCalcGUI dsp = new SimpleCalcGUI();
+    }
 }
+
