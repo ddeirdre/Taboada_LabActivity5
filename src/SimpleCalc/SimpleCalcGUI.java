d94c0e723206e46904446f212e1de4f1c450adc9
Charlotte Taboada <lovdeirdre@gmail.com>
2022-12-17 13:45:06 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,7 +1,5 @@
 package SimpleCalc;
 
-import LeapYear.LeapYearGUI;
-
 import javax.swing.*;
 
 public class SimpleCalcGUI extends JFrame {
@@ -10,10 +8,48 @@ public class SimpleCalcGUI extends JFrame {
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
-    private JLabel lblResult;
+    private JLabel label;
+    private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+        setTitle("Simple Calculator");
+        btnCompute.addActionListener((event) ->{
+            try {
+                double n1 = Double.parseDouble(tfNumber1.getText());
+                double n2 = Double.parseDouble(tfNumber2.getText());
+                String ops = (String) cbOperations.getSelectedItem();
+
+                double res = 0;
+                {
+                    switch (ops) {
+                        case "+" -> res = n1 + n2;
+                        case "-" -> res = n1 - n2;
+                        case "*" -> res = n1 * n2;
+                        case "/" -> {
+                            if (n2 == 0) {
+                                throw new ArithmeticException("Divisor cannot be 0");
+                            }
+                            res = n1 / n2;
+                        }
+                    }
+                }
+
+                lblResult.setText(Double.toString(res));
+            } catch (NumberFormatException boot2) {
+                JOptionPane.showMessageDialog(SimpleCalcGUI.this, "Invalid Input", "Error", JOptionPane.ERROR_MESSAGE);
+            } catch (ArithmeticException div){
+                JOptionPane.showMessageDialog(SimpleCalcGUI.this, "Cannot be divided by 0", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI dsp = new SimpleCalcGUI();
+        dsp.setContentPane(new SimpleCalcGUI().panel1);
+        dsp.setSize(550, 400);
+        dsp.setVisible(true);
     }
 }
 
